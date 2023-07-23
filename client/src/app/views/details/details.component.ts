b93bb7a0333e9aa1e16042059eff92a162178420
Radoslav Petrov <115632451+AbyssKnight7aken@users.noreply.github.com>
2023-07-23 21:24:48 +0300

    progress to details page

--- client/src/app/views/details/details.component.ts
+++ client/src/app/views/details/details.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { Log } from 'src/app/types/log';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  constructor(private apiService: ApiService, private router: Router, private activatedRoute: ActivatedRoute,) { }
 
+  log:any
+
+  getImageAsBase64(): string {
+    let binary = '';
+    const bytes = new Uint8Array(this.log.img.data.data);
+    
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return btoa(binary);
+  }
+
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['logId'];
+    this.apiService.getDetails(id).subscribe(
+      {
+        next: (result) => {
+          this.log = result;
+
+          
+          console.log(this.log);
+        },
+        error: (err) => {
+          console.error(`Error: ${err}`);
+        }
+      }
+    );
+  }
 }
